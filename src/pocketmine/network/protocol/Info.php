1cbacc652ded851eeb4d63350b494bce17614a8e
Wili Rodin <paypalwilirodin@gmail.com>
2017-08-27 14:56:35 -0500

    Version network 1.2.0.22

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -33,7 +33,7 @@ interface Info {
 	const CURRENT_PROTOCOL = 131;
 	const ACCEPTED_PROTOCOLS = [131];
 	const MINECRAFT_VERSION = ["v1.2.0"];
-	const MINECRAFT_VERSION_NETWORK = "1.2.0.7";
+	const MINECRAFT_VERSION_NETWORK = "1.2.0.22";
 
 	const LOGIN_PACKET = 0x01;
 	const PLAY_STATUS_PACKET = 0x02;
